f5f5cfc8b2ca0fbbdc55aa5e5f7a0b829c10102d
Guillermo Ríos <grios@stratio.com>
2020-09-22 18:48:06 +0200

    Add comment

--- project.clj
+++ project.clj
@@ -87,7 +87,9 @@
      :exclusions [com.fasterxml.jackson.core/jackson-core]]
    ;; STRATIO >
    [com.mattbertolini/liquibase-slf4j "2.0.0"]                        ; Java Migrations lib logging. We don't actually use this AFAIK (?)
+   ;; < STRATIO - bump nippy to 2.15.0 due to vulnerability
    [com.taoensso/nippy "2.15.0"]                                      ; Fast serialization (i.e., GZIP) library for Clojure
+   ;; STRATIO >
    [commons-codec/commons-codec "1.12"]                               ; Apache Commons -- useful codec util fns
    [commons-io/commons-io "2.6"]                                      ; Apache Commons -- useful IO util fns
    [commons-validator/commons-validator "1.6"                         ; Apache Commons -- useful validation util fns
