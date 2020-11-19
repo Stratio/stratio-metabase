bdeeff9700f73f927b088177a7ad6d8d0f4a5aa2
Frannie-Ludmilla <fpesci@stratio.com>
2020-11-19 10:56:12 +0100

    Apply required changes

--- src/metabase/stratio/constraints.clj
+++ src/metabase/stratio/constraints.clj
@@ -1,22 +1,23 @@
 (ns metabase.stratio.constraints
-  "Adds a limit to the maximum number of exported rows as a constraint if the variable STRATIO_ABSOLUTE_MAX_RESULTS is defined.")
-
+  "Adds a limit to the maximum number of exported rows as a constraint if the variable STRATIO_ABSOLUTE_MAX_RESULTS is defined."
+  (:require [metabase
+             [config :as config]]))
 
 (def ^:private max-results-stratio
-  "General maximum number of rows to return from an API query. Returns nil if not defined or not parsable to Long"
-  (try (let [s (System/getenv "STRATIO_ABSOLUTE_MAX_RESULTS")] (Long/valueOf s)) (catch Exception _)))
+  "General maximum number of rows to return from an API query.
+  Returns nil if STRATIO_ABSOLUTE_MAX_RESULTS is not defined or not parsable to a number"
+  (config/config-int :stratio-absolute-max-results))
 
 (def default-query-constraints-stratio
   "Default map of constraints that we apply on dataset queries executed by the api.
   The keywords max-results and max-results-bare-rows can have the same value as shown in add-row-count-and-status"
-  (let [parsed-limit  (or max-results-stratio metabase.query-processor.interface/absolute-max-results)]
-   {:max-results           parsed-limit
-    :max-results-bare-rows parsed-limit}))
+  (when max-results-stratio
+   {:max-results           max-results-stratio
+    :max-results-bare-rows max-results-stratio}))
 
 (defn add-query-constraints-stratio
-  "If defined via environment variable"
+  "If the environment variable is defined, add the constraints"
   [query]
   (cond-> query
           max-results-stratio (assoc :constraints default-query-constraints-stratio)))
-
 
