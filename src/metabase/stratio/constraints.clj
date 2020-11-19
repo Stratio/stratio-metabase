fae03734218da641dfd80b42af126ebe19929e69
Frannie-Ludmilla <fpesci@stratio.com>
2020-11-19 15:25:36 +0100

    Refactor inline

--- src/metabase/stratio/constraints.clj
+++ src/metabase/stratio/constraints.clj
@@ -8,17 +8,10 @@
   Returns nil if STRATIO_ABSOLUTE_MAX_RESULTS is not defined or not parsable to a number"
   (config/config-int :stratio-absolute-max-results))
 
-(defn default-query-constraints-stratio
-  "Default map of constraints that we apply on dataset queries executed by the api.
-  The keywords max-results and max-results-bare-rows can have the same value as shown in add-row-count-and-status"
-  [max-results]
-  (when max-results
-   {:max-results           max-results
-    :max-results-bare-rows max-results}))
-
 (defn add-query-constraints-stratio
   "If the environment variable is defined, add the constraints"
   [query]
   (cond-> query
-          max-results-stratio (assoc :constraints (default-query-constraints-stratio max-results-stratio))))
-
+          max-results-stratio (assoc :constraints
+                                 {:max-results           max-results-stratio
+                                  :max-results-bare-rows max-results-stratio})))
