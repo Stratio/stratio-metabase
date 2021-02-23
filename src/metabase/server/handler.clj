52b2e00c628e32b036597866fe68aa6e6e232eeb
Guillermo Ríos <grios@stratio.com>
2021-02-23 12:21:22 +0100

    Auto login via headers

--- src/metabase/server/handler.clj
+++ src/metabase/server/handler.clj
@@ -10,6 +10,9 @@
             [metabase.server.middleware.security :as mw.security]
             [metabase.server.middleware.session :as mw.session]
             [metabase.server.middleware.ssl :as mw.ssl]
+            ;; < STRATIO - auto login from headers info
+            [metabase.stratio.auth :as st.auth]
+            ;; STRATIO />
             [metabase.server.routes :as routes]
             [ring.middleware.cookies :refer [wrap-cookies]]
             [ring.middleware.gzip :refer [wrap-gzip]]
@@ -26,6 +29,9 @@
    #'mw.exceptions/catch-api-exceptions      ; catch exceptions and return them in our expected format
    #'mw.log/log-api-call
    #'mw.security/add-security-headers        ; Add HTTP headers to API responses to prevent them from being cached
+   ;; < STRATIO - auto login from headers info
+   #'st.auth/forbid-editing-username         ; when auto-login enabled, respond with a 403 requests to edit user name
+   ;; STRATIO />
    #'mw.json/wrap-json-body                  ; extracts json POST body and makes it avaliable on request
    #'mw.json/wrap-streamed-json-response     ; middleware to automatically serialize suitable objects as JSON in responses
    #'wrap-keyword-params                     ; converts string keys in :params to keyword keys
@@ -33,6 +39,9 @@
    #'mw.misc/maybe-set-site-url              ; set the value of `site-url` if it hasn't been set yet
    #'mw.session/bind-current-user            ; Binds *current-user* and *current-user-id* if :metabase-user-id is non-nil
    #'mw.session/wrap-current-user-info       ; looks for :metabase-session-id and sets :metabase-user-id and other info if Session ID is valid
+   ;; < STRATIO - auto login from headers info
+   #'st.auth/auto-login                      ; if we cannot find a session-id look for user info in headers and create user and session
+   ;; STRATIO />
    #'mw.session/wrap-session-id              ; looks for a Metabase Session ID and assoc as :metabase-session-id
    #'mw.auth/wrap-api-key                    ; looks for a Metabase API Key on the request and assocs as :metabase-api-key
    #'wrap-cookies                            ; Parses cookies in the request map and assocs as :cookies
